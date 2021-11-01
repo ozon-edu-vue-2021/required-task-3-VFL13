0b51fae7e691653ab90686eeecb062aacf1fc367
I.Sosedin <i.sosedin@skoltech.ru>
2021-11-01 14:54:21 +0300

    First

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-    <div id="app">
-        <div class="office">
-            <Map />
-            <SideMenu />
-        </div>
+  <div id="app">
+    <div class="office">
+      <Map @show-user-info="ShowUserInfo" @hide-user-info="HideUserInfo" />
+      <SideMenu
+        :is-user-opened="isUserOpened"
+        :person="person"
+        @hide-user-info="HideUserInfo"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 import Map from "./components/Map.vue";
 import SideMenu from "./components/SideMenu.vue";
+import people from "./assets/data/people.json";
 
 export default {
   name: "App",
@@ -17,40 +22,56 @@ export default {
     Map,
     SideMenu,
   },
+  data() {
+    return {
+      isUserOpened: false,
+      person: null,
+    };
+  },
+  methods: {
+    ShowUserInfo(user_id) {
+      this.person = people.find((it) => it._id === user_id);
+      this.isUserOpened = true;
+    },
+    HideUserInfo() {
+      this.isUserOpened = false;
+      this.person = null;
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background-color: #fafafa;
-    padding: 24px;
-    box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-color: #fafafa;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
 html,
 body,
 #app {
-    height: 100%;
+  height: 100%;
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0;
 }
 
 .office {
-    display: grid;
-    grid-template-columns: 1fr 320px;
-    border-radius: 6px;
-    border: 1px solid #ccd8e4;
-    height: 100%;
-    background: white;
-    max-width: 1500px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 320px;
+  border-radius: 6px;
+  border: 1px solid #ccd8e4;
+  height: 100%;
+  background: white;
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,124 @@
 <template>
-    <div class="map">
-        <h3>Карта офиса</h3>
+  <div class="map">
+    <h3>Карта офиса</h3>
 
-        <div
-            v-if="!isLoading"
-            class="map-root"
-        >
-            <!-- map -->
-        </div>
-        <div v-else>Loading...</div>
+    <div v-if="!isLoading" class="map-root">
+      <!-- map -->
+      <MapSVG ref="mapSvg" />
+      <WorkPlaceSVG ref="placeSVG" v-show="false" />
     </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
+import * as d3 from "d3";
+import tables from "../assets/data/tables.json";
+import legend from "../assets/data/legend.json";
+import MapSVG from "../assets/images/map.svg";
+import WorkPlaceSVG from "../assets/images/workPlace.svg";
+
 export default {
-    data() {
-        return {
-            isLoading: false,
-        };
+  components: {
+    MapSVG,
+    WorkPlaceSVG,
+  },
+  data() {
+    return {
+      isLoading: false,
+      svgMap: null,
+      gMap: null,
+      svgWorkPlace: null,
+      workPlaces: [],
+    };
+  },
+  mounted() {
+    // Выбираем svg с картой и вешаем обработчик клика.
+    this.svgMap = d3.select(this.$refs.mapSvg);
+    this.svgMap.on("click", this.clickBG);
+    this.gMap = this.svgMap.select("g");
+    this.workPlaces = tables;
+
+    this.svgWorkPlace = d3.select(this.$refs.placeSVG);
+
+    if (this.gMap) {
+      this.drawTable();
+    } else {
+      console.error("map svg don't have g element");
+    }
+  },
+  methods: {
+    clickBG() {
+      this.$emit("hide-user-info");
     },
+    clickWP(user_id) {
+      // При клике на рабочее место, передаем его ID.
+      this.$emit("show-user-info", user_id);
+    },
+    drawTable() {
+      // создаем группу рабочих мест
+
+      const svgWorkPlacesGroup = this.gMap
+        .append("g")
+        .classed("groupPlaces", true);
+
+      // В группе размещаем рабочие места, стилизуем, добавляем обработчик клика.
+
+      this.workPlaces.map((place) => {
+        svgWorkPlacesGroup
+          .append("g")
+          .attr(
+            "transform",
+            `translate(${place.x}, ${place.y}) rotate(${
+              place.rotate || 0
+            }) scale(0.5)`
+          )
+          .attr("id", place.id)
+          .classed("employer-place", true)
+          .attr("group_id", place.group_id)
+          .html(this.svgWorkPlace.html())
+          .attr(
+            "fill",
+            legend.find((it) => it.group_id === place.group_id)?.color
+          )
+          .on("click", (event) => {
+            event.stopPropagation();
+            this.clickWP(place._id);
+          });
+      });
+    },
+  },
 };
 </script>
 
 <style scoped>
 .map {
-    height: 100%;
-    width: 100%;
-    padding: 24px;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 24px;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 }
 
 .map-root {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0;
 }
 
 ::v-deep svg {
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 }
 
 ::v-deep .table {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
